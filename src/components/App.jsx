7940bdce5f8377ec42eada6b0668bb96f6813237
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2024-04-02 20:07:20 +0100

    Initial environment set up
    I've installed TailwindCSS, so lets see how we get on with that

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+// import { useState } from 'react'
+import "../styles/App.css";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-lime-800">Hello world!</h1>
+  );
+}
