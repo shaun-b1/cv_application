fc7fcd329b30b06fcdea13e5038011d1b15f9e33
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2024-04-04 14:16:08 +0100

    Main form fields built
    Form <fieldset>s built, I'm thinking I could probably simplyfy them down to three blocks and one section, as currently they are three sections and two blocks. I've implemented a button to create new blocks in the Education and Work sections, and need to add corresponding delete buttons with uuid(). Then I need to add in states to hold all the information

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-export default function Button({ onClick, children }) {
+export default function Button({ classes, onClick, children }) {
   return (
     <button
-      className="text-slate-700 bg-slate-100 rounded-md border-slate-300 px-8 py-2"
+      className={`text-slate-700 bg-slate-100 rounded-md border-slate-300 px-8 py-2 ${classes}`}
       onClick={onClick}
     >
       {children}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
-// import Button from "./Button";
+import { useState } from "react";
+import Button from "./Button";
 
 function PersonalInformationSection() {
   return (
@@ -40,10 +41,134 @@ function PersonalInformationSection() {
   );
 }
 
+function EducationSection() {
+  const [education, setEducation] = useState([]);
+
+  const addField = (e) => {
+    e.preventDefault();
+    const newField = (
+      <li className="grid grid-cols-2 gap-2" key={education.length}>
+        <EducationBlock />
+      </li>
+    );
+    setEducation([...education, newField]);
+  };
+
+  return (
+    <fieldset className="border border-slate-700 rounded-md p-4 m-4">
+      <legend className="text-xl">Education History</legend>
+      <ul>{education.map((item) => item)}</ul>
+      <Button classes={"mt-4"} onClick={addField}>
+        Add Education
+      </Button>
+    </fieldset>
+  );
+}
+
+function EducationBlock() {
+  return (
+    <>
+      <label>
+        <p>Degree</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="text"
+        />
+      </label>
+      <label>
+        <p>School</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="text"
+        />
+      </label>
+      <label>
+        <p>Start Date</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="date"
+        />
+      </label>
+      <label>
+        <p>End Date</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="date"
+        />
+      </label>
+    </>
+  );
+}
+
+function WorkSection() {
+  const [work, setWork] = useState([]);
+
+  const addField = (e) => {
+    e.preventDefault();
+    const newField = (
+      <li className="grid grid-cols-2 gap-2" key={work.length}>
+        <WorkBlock />
+      </li>
+    );
+    setWork([...work, newField]);
+  };
+
+  return (
+    <fieldset className="border border-slate-700 rounded-md p-4 m-4">
+      <legend className="text-xl">Work History</legend>
+      <ul>{work.map((item) => item)}</ul>
+      <Button classes={"mt-4"} onClick={addField}>
+        Add Work
+      </Button>
+    </fieldset>
+  );
+}
+
+function WorkBlock() {
+  return (
+    <>
+      <label>
+        <p>Role</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="text"
+        />
+      </label>
+      <label>
+        <p>Employer</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="text"
+        />
+      </label>
+      <label>
+        <p>Start Date</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="date"
+        />
+      </label>
+      <label>
+        <p>End Date</p>
+        <input
+          className="bg-slate-100 rounded-md border-slate-300 text-slate-700"
+          type="date"
+        />
+      </label>
+      <label className="col-span-2">
+        <p>Responsibilties: </p>
+        <textarea className="bg-slate-100 rounded-md border-slate-300 w-full text-slate-700" />
+      </label>
+    </>
+  );
+}
+
 export default function Form() {
   return (
     <form className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-slate-100">
       <PersonalInformationSection />
+      <EducationSection />
+      <WorkSection />
     </form>
   );
 }
